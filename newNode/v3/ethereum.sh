f99abe740e343c3fe23e9be406cb51486e311ce3
TitanLi <Lisheng0706@gmail.com>
2018-07-19 17:29:30 +0800

    Add new node version 3

--- newNode/v3/ethereum.sh
+++ newNode/v3/ethereum.sh
@@ -5,7 +5,30 @@ ipInfo=$(echo $addressInfo | cut -d'/' -f 1)
 
 echo $ipInfo
 
-echo $(cat /eth-net-intelligence-api/app.json | jq '.[0].env.RPC_HOST="'$ipInfo'"')
+jq '.[0]' "/eth-net-intelligence-api/app.json"
+echo $(jq '.[0].env.RPC_HOST="'$ipInfo'"' /eth-net-intelligence-api/app.json) > /eth-net-intelligence-api/app.json
+
+# sudo docker run -tid -h test --name test -v /home/titan/Titan/github/blockchain/eth-poc/newNode/volumes:/volumes ubuntu:16.04 /bin/bash
+hostname=$(cat /etc/hostname)
+echo $(jq '.[0].name="'$hostname'"' /eth-net-intelligence-api/app.json) > /eth-net-intelligence-api/app.json
+
+INSTANCE_NAME=$(jq ".$hostname.INSTANCE_NAME" /volumes/initData.json)
+echo $(jq ".[0].env.INSTANCE_NAME = $INSTANCE_NAME" /eth-net-intelligence-api/app.json) > /eth-net-intelligence-api/app.json
+
+WS_SERVER=$(jq ".$hostname.WS_SERVER" /volumes/initData.json)
+echo $(jq ".[0].env.WS_SERVER = $WS_SERVER" /eth-net-intelligence-api/app.json) > /eth-net-intelligence-api/app.json
+
+WS_SECRET=$(jq ".$hostname.WS_SECRET" /volumes/initData.json)
+echo $(jq ".[0].env.WS_SERVER = $WS_SECRET" /eth-net-intelligence-api/app.json) > /eth-net-intelligence-api/app.json
+
+puppethFrom=$(jq ".$hostname.puppethFrom" /volumes/initData.json)
+
+puppeth=$(jq ".$hostname.puppeth" /volumes/initData.json)
+
+puppethPath=$(echo $puppethFrom | sed 's/"//g')
+puppethFileName=$(echo $puppeth | sed 's/"//g')
+
+cp $puppethPath $puppethFileName
 
 pm2 start /eth-net-intelligence-api/app.json
 
--- newNode/v3/test/jq/test.sh
+++ newNode/v3/test/jq/test.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
 # echo $(jq '.address1 = "abcde"' startData.json) > endData.json
-data=$(jq '.test.INSTANCE_NAME' startData.json)
+# data=$(jq '.test.INSTANCE_NAME' startData.json)
 
-echo $data
-echo $(jq ".INSTANCE_NAME = $data" endData.json) > endData.json
+# echo $data
+# echo $(jq ".INSTANCE_NAME = $data" endData.json) > endData.json
+
+# hostname="titan"
+# echo $hostname
+
+# jq ".$hostname.INSTANCE_NAME" startData.json
+
+puppethFrom=$(jq ".test.puppethFrom" /home/titan/Titan/github/blockchain/eth-poc/newNode/volumes/initData.json)
+
+asd=$(echo $puppethFrom | sed 's/"//g')
+
+echo $asd
+
+a="apple"
+echo $a
